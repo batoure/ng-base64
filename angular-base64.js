7e9f1fe9cf91914845ddb091b70ee4023a24a3dd
Leahy <leahy@apache.org>
2017-03-28 03:15:03 -0700

    Fixed minification and versioning

--- angular-base64.js
+++ angular-base64.js
@@ -1,3 +1,12 @@
+
+(function(){
+  'use strict';
+
+  // Prepare the 'ngBase64' module for subsequent registration of controllers and delegates
+   angular.module('ngBase64', []);
+
+
+})();
 (function(){
   'use strict';
 
