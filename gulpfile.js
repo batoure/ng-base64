0b63e74595e09de37df096749faf8b1502742f63
Leahy <leahy@apache.org>
2017-03-29 20:31:25 -0700

    npm build action for gulp

--- gulpfile.js
+++ gulpfile.js
@@ -16,3 +16,14 @@ gulp.task('concat-js', function () {
     .pipe(concat('angular-base64.js'))  // concat and name it "concat.js"
     .pipe(gulp.dest('./'));
 });
+
+gulp.task('build-js', function () {
+    gulp.src('./src/*/*.js') // path to your files
+    .pipe(uglify())// minify and strip comments from the code"
+    .pipe(concat('angular-base64.min.js'))  // concat and name it
+    .pipe(gulp.dest('./'));
+    
+    gulp.src('./src/*/*.js') // path to your files
+    .pipe(concat('angular-base64.js'))  // concat and name it "concat.js"
+    .pipe(gulp.dest('./'));
+});
